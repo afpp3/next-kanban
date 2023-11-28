52412753f02fb08a32c0ce4bd7768fa47a07de47
Andre Felippe <afelipp3@gmail.com>
2023-11-28 03:12:09 -0300

    feat: create logo component

---
+++ components/logo.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import localFont from 'next/font/local'
+
+import { cn } from '@/lib/utils'
+
+const headingFonts = localFont({
+  src: '../public/fonts/font.woff2',
+})
+
+export default function Logo() {
+  return (
+    <Link href="/">
+      <div className="hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
+        <Image src="/logo.svg" alt="Taskify Logo" height={30} width={30} />
+        <p
+          className={cn(
+            'pb-1 text-lg text-neutral-700',
+            headingFonts.className
+          )}
+        >
+          Taskify
+        </p>
+      </div>
+    </Link>
+  )
+}
