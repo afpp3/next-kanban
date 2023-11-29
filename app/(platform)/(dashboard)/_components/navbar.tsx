ed3abe6a01e6e9948d3c7ed933002f8f004dd424
Andre Felippe <afelipp3@gmail.com>
2023-11-29 00:51:40 -0300

    feat(dashboard): init dashboard layout with navbar

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,59 @@
+import { Plus } from 'lucide-react'
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
+
+import { Logo } from '@/components/logo'
+import { Button } from '@/components/ui/button'
+
+export function Navbar() {
+  return (
+    <nav className="fixed top-0 z-20 flex h-14 w-full items-center border-b bg-white px-4 shadow-sm">
+      {/* TODO: mobile sidebar */}
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex ">
+          <Logo />
+        </div>
+
+        <Button
+          size="sm"
+          className="hidden h-auto rounded-sm px-2 py-1.5 md:block"
+        >
+          Create
+        </Button>
+
+        <Button size="sm" className="block rounded-sm md:hidden">
+          <Plus className="h-4 w-4" />
+        </Button>
+      </div>
+
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="organization/:id"
+          afterSelectOrganizationUrl="organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+            },
+          }}
+        />
+
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
+        />
+      </div>
+    </nav>
+  )
+}
