ec11a51dc2871fcb956194827e30f5a9f6cb0faa
Andre <afelipp3@gmail.com>
2024-01-05 02:43:22 -0300

    fix: url on sign in and sign up links

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -11,11 +11,11 @@ export function Navbar() {
 
         <div className="flex w-full items-center justify-between space-x-4 md:block md:w-auto">
           <Button size="sm" variant="outline" asChild>
-            <Link href="sign-in">Login</Link>
+            <Link href="/sign-in">Login</Link>
           </Button>
 
           <Button size="sm" asChild>
-            <Link href="sign-up">Get Taskify for free</Link>
+            <Link href="/sign-up">Get Taskify for free</Link>
           </Button>
         </div>
       </div>
